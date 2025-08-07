6a8fc99a61184aed764d8ee947a0b54bc3410bf8
faturamento3 <faturamento3@Agromercantil.local>
2025-08-06 21:07:32 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import plotly as pl
+import psycopg2
+
+# estabelecer conexão com o banco de dados
+
+conn = psycopg2.connectect(
+    host="localhost",
+    port="5435",
+    dbname="nome_do_banco",
+    user="seu_usuario",
+    password="sua_senha"
+)
